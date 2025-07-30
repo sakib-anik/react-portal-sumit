fc8f603c606be422639680043fa91aa12798092f
MD. NAZMUS SAKIB <sakib.within@gmail.com>
2025-07-31 03:44:48 +0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import { createPortal } from 'react-dom';
+
+const sidebarContentEl = document.getElementById('sidebar-content');
+
+export default function App() {
+  return (
+    <>
+      <MainContent />
+      {createPortal(
+        <SidebarContent />,
+        sidebarContentEl
+      )}
+    </>
+  );
+}
